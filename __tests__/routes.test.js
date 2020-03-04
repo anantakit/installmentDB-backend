09ed4ee5f057c99d6473d7716a6f149ec6ee58b5
anantakit <frame.zz@hotmail.com>
2020-03-04 14:46:35 +0700

    create test search firstname

--- __tests__/routes.test.js
+++ __tests__/routes.test.js
@@ -1,7 +1,15 @@
-const { app, start } = require("../src/routes/routes");
 const supertest = require("supertest");
-test("test get user route", async () => {
+const mongoose = require("mongoose");
+const { app, start } = require("../src/routes/routes");
+
+beforeEach(async () => {
   await start();
+});
+afterEach(async () => {
+  await mongoose.connection.close();
+});
+
+test("test get user route", async () => {
   const response = await supertest(app).get("/users");
 
   expect(response.status).toEqual(200);
@@ -9,7 +17,6 @@ test("test get user route", async () => {
 });
 
 test("test get finacials route", async () => {
-  await start();
   const response = await supertest(app).get("/finacials");
 
   expect(response.status).toEqual(200);
@@ -17,9 +24,15 @@ test("test get finacials route", async () => {
 });
 
 test("test get transactions route", async () => {
-  await start();
   const response = await supertest(app).get("/transactions");
 
   expect(response.status).toEqual(200);
   //expect(response.body.length).toEqual(6);
+});
+
+test("test search by firstName", async () => {
+  const response = await supertest(app).get("/users/ang");
+
+  expect(response.status).toEqual(200);
+  expect(response.body[0].firstName).toEqual("Angkana");
 });
